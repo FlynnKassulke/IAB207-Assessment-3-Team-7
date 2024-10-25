275dbb054c158dcc6ff27cdcb2b7e7f5a036a063
Nidhi Chitti <nchitti707@gmail.com>
2024-10-25 19:18:43 +1000

    Added contact number and street address

--- website/models.py
+++ website/models.py
@@ -9,6 +9,8 @@ class User(db.Model, UserMixin):
     name = db.Column(db.String(100),index=True,unique=True,nullable=False)
     emailid = db.Column(db.String(100), index=True, nullable=False)
     password_hash = db.Column(db.String(255),nullable=False)
+    contact_number = db.Column(db.Integer(10), index=True, nullable=False)
+    street_address = db.Column(db.String(100), index=True, nullable=False)
 
     comments = db.relationship('Comment',backref='user')
     events = db.relationship('Event',secondary='comments',backref=db.backref('commented_users'))
@@ -21,6 +23,8 @@ class Event(db.Model):
     description=db.Column(db.String(1000), nullable=False)
     location=db.Column(db.String(200),nullable=False)
     time=db.Column(db.DateTime,nullable=False)
+    contact_number = db.Column(db.Integer(10), index=True, nullable=False)
+    street_address = db.Column(db.String(100), index=True, nullable=False)
 
     comments = db.relationship('Comment',backref='events')
     orders = db.relationship('Order', backref='events')
